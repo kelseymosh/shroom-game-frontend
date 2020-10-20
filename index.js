db9b19ce5bfe61606339138eda6ea476278f8278
Kelsey Oshiro <kelseymosh@gmail.com>
2020-10-20 13:44:04 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('test')
+
+const mainGamePage = document.getElementById("main-game-page")
+
+// fetch('http://localhost:3000/mushroomsrandom')
+//     .then(response => response.json())
+//     .then(mushroom => {
+//         const photoCard = document.createElement('h2').setAttribute("src", `${mushroom.img_url}`)
+//         photoCard.
+//     }
+
